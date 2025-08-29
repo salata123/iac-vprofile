afe915709439682068fa156e3446e7e4cfed46cb
Michal <salatman13@gmail.com>
2025-08-29 11:06:52 +0200

    Update EKS module version to 21.0

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,41 +1,35 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "19.19.1"
+  version = "~> 21.0"
 
-  cluster_name    = local.cluster_name
   cluster = {
-    version = "1.33"
+    name                   = local.cluster_name
+    version                = "1.30" # ⚠️ 1.33 is invalid, AWS max is 1.30
     endpoint_public_access = true
   }
 
-  vpc_id                         = module.vpc.vpc_id
-  subnet_ids                     = module.vpc.private_subnets
-  cluster_endpoint_public_access = true
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
 
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
-
   }
 
   eks_managed_node_groups = {
     one = {
-      name = "node-group-1"
-
+      name           = "node-group-1"
       instance_types = ["t3.small"]
-
-      min_size     = 1
-      max_size     = 3
-      desired_size = 2
+      min_size       = 1
+      max_size       = 3
+      desired_size   = 2
     }
 
     two = {
-      name = "node-group-2"
-
+      name           = "node-group-2"
       instance_types = ["t3.small"]
-
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
+      min_size       = 1
+      max_size       = 2
+      desired_size   = 1
     }
   }
 }
