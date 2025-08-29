e71e30bfd1a5a48e10fcb60ceaad631db4bd2b7e
Michal <salatman13@gmail.com>
2025-08-29 11:10:47 +0200

    Format alignment in EKS module configuration for consistency

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -2,10 +2,10 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 21.0"
 
-  name                = local.cluster_name
-  kubernetes_version  = "1.30"
+  name               = local.cluster_name
+  kubernetes_version = "1.30"
 
-  endpoint_public_access = true
+  endpoint_public_access                   = true
   enable_cluster_creator_admin_permissions = true
 
   vpc_id     = module.vpc.vpc_id
